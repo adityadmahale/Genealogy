ee04000c2994f733c1656b21baa31c84a7bf0246
Aditya Mahale <aditya.m258@gmail.com>
2021-11-17 14:40:39 +0530

    Create MediaManagement class for managing media related information

---
+++ src/MediaManagement.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Map;
+
+public class MediaManagement {
+	
+	FileIdentifier addMediaFile(String fileLocation) {
+		return null;
+	}
+	
+	Boolean recordMediaAttributes(FileIdentifier fileIdentifier, Map<String, String> attributes) {
+		return false;
+	}
+	
+	Boolean peopleInMedia(FileIdentifier fileIdentifier, List<PersonIdentity> people ) {
+		return false;
+	}
+	
+	Boolean tagMedia(FileIdentifier fileIdentifier, String tag) {
+		return false;
+	}
+}
