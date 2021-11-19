fd99aff80741c892e902919cf0b6b4f7576e20db
Aditya Mahale <aditya.m258@gmail.com>
2021-11-19 14:42:19 +0530

    Include fields, getters and setters in the FileIdentifier class

--- src/FileIdentifier.java
+++ src/FileIdentifier.java
@@ -1,4 +1,18 @@
 
 public class FileIdentifier {
+	private int fileId;
+	private String fileLocation;
+	
+	public FileIdentifier(int fileId, String fileLocation) {
+		this.fileId = fileId;
+		this.fileLocation = fileLocation;
+	}
 
+	public int getFileId() {
+		return fileId;
+	}
+
+	public String getFileLocation() {
+		return fileLocation;
+	}
 }
