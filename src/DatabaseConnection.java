1d26118042055b621f8440b8bee451d794638f87
Aditya Mahale <aditya.m258@gmail.com>
2021-12-04 13:37:51 +0530

    Store database inputs in a properties file

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,23 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 // Singleton class for getting database connection
 class DatabaseConnection {
+	private static final String CONNECTION_STRING_KEY = "connection_string";
+	private static final String USERNAME_KEY = "username";
+	private static final String PASSWORD_KEY = "password";
 	
 	// Connection object shared across all the code
 	private static Connection connection;
 	
 	static Connection getConnection() {
+		
 		try {
 			// Returns the connection object if it's not null
 			if (connection != null) {
 				return connection;
 			}
 			
+			// Get database inputs from the properties file
+			Properties properties = Utility.loadDatabaseInputs();
+			String connectionString = properties.getProperty(CONNECTION_STRING_KEY);
+			String username = properties.getProperty(USERNAME_KEY);
+			String password = properties.getProperty(PASSWORD_KEY);
+			
 			// Establish connection to the database
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/genealogy", "root", "pulsar25");
+			connection = DriverManager.getConnection(connectionString, username, password);
 			
 		} catch (SQLException | ClassNotFoundException e) {
 			throw new IllegalStateException(e.getMessage());
--- src/Utility.java
+++ src/Utility.java
@@ -1,8 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Properties;
 
 class Utility {
+	private static final String propertiesFilePath = "database.properties";
+	
 	static void updateParentChildRelationship(PersonIdentity parent, PersonIdentity child) {
 		// Add child for the individual and the partner
 		parent.addChild(child);
@@ -42,4 +48,19 @@ class Utility {
         }
         return true;
     }
+	
+	static Properties loadDatabaseInputs() {
+		Properties properties = new Properties();
+		try {
+			FileReader read = new FileReader(propertiesFilePath);
+			properties.load(read);
+			
+		} catch (FileNotFoundException e) {
+			throw new IllegalStateException(e.getMessage());
+		} catch (IOException e) {
+			throw new IllegalStateException(e.getMessage());
+		}
+		
+		return properties;
+	}
 }
